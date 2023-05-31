169b35987ad072d767051f08ccc1730b3f60e6a4
Nathy <91018534+nathyfaculade@users.noreply.github.com>
2023-05-30 21:56:25 -0300

    CepFragment2

--- lib/pages/cep_fragment.dart
+++ lib/pages/cep_fragment.dart
@@ -2,6 +2,8 @@ import 'package:api_md/services/cep_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
+import '../model/cep_model.dart';
+
 class CepFragment extends StatefulWidget{
   static const title = 'Consulta Cep';
 
@@ -14,9 +16,14 @@ class _CepFragmentState extends State<CepFragment>{
   final _service = CepService();
   final _controler = TextEditingController();
   final _formKey = GlobalKey<FormState>();
-  final _cepFormater = MaskTextInputFormatter();
+  final _cepFormater = MaskTextInputFormatter(
+    mask: '#####-###',
+    filter: {'#' : RegExp(r'[0-9]')}
+  );
+  var _loading = false;
+  Cep? _cep;
 
   Widget build(BuildContext context){
-    return
+    return Container();
   }
 }
